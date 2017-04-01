93634ec1b11c77a5b86e760c291588aca1d67cfa
Muhsin Ali <muhsinali@users.noreply.github.com>
2017-04-01 22:26:58 +0100

    Clean up Game

--- app/models/Game.scala
+++ app/models/Game.scala
@@ -8,20 +8,9 @@ package models
 * */
 class Game {
   val grid = new Grid
-
   val p1 = new Player("Player 1", Disc.RED)
   val p2 = new Player("Player 2", Disc.YELLOW)
   var currentPlayer: Player = p1
-
-  // Test configuration
-  val disc = Disc.RED
-  val other = Disc.YELLOW
-  for(c <- 0 until WinCondition.NUM_CONSECUTIVE){
-    for(_ <- 0 until c){
-      grid.placeDisc(c, other)
-    }
-    if(c != WinCondition.NUM_CONSECUTIVE - 1) grid.placeDisc(c, disc)
-  }
 
 
   def hasBeenWon: Boolean = WinCondition.hasGameBeenWon(grid, currentPlayer.disc)
